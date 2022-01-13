074f67afab004fcf32c578feec7985ed1b0b9dd0
Naomi Valdovinos <n.valdovinos74@gmail.com>
2022-01-13 17:41:39 -0500

    adding changes to folder

--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -51,7 +51,7 @@ const Contact = () => {
           <a target='_blank' rel="noreferrer" href="tel:919-223-8501" class="phoneNumber"><BsPhone /></a>
           <a target='_blank' rel="noreferrer" href="mailto:n.valdovinos74@gmail.com" class="email"><BsFillEnvelopeOpenFill /></a>
           <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/naomi-valdovinos-0185bb191/"class="linkedin"><FaLinkedin /></a>
-          <a target='_blank' rel="noreferrer" href="https://github.com/Nvaldovinos" class="gitHub"><FaGithub /></a>
+          <a target='_blank' rel="noreferrer" href="https://github.com/Nvaldovinos" class="gitHub"><FaGithub/></a>
         </div>
           {/* <div class="col-lg-5 col-md-8">
             <form class="php-email-form" id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
