ff884b9c673759f31cc66b40a099508cca6c7590
Naomi Valdovinos <n.valdovinos74@gmail.com>
2022-01-13 21:16:41 -0500

    finished editing picures

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -7,7 +7,13 @@ function Footer() {
     return (
       <footer id="footer">
       <div class="container">
-      <div class="copyright">
+      {/* <div class="social-links">
+          <a target='_blank' rel="noreferrer" href="tel:919-223-8501" class="phoneNumber"><BsPhone /></a>
+          <a target='_blank' rel="noreferrer" href="mailto:n.valdovinos74@gmail.com" class="email"><BsFillEnvelopeOpenFill /></a>
+          <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/naomi-valdovinos-0185bb191/"class="linkedin"><FaLinkedin /></a>
+          <a target='_blank' rel="noreferrer" href="https://github.com/Nvaldovinos" class="gitHub"><FaGithub/></a>
+        </div> */}
+        <div class="copyright">
           &copy; Copyright <strong><span>Naomi Valdovinos</span></strong>. All Rights Reserved
         </div>
       </div>
--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -1,12 +1,10 @@
 import '../styles/Style.css';
 import zeal from '../../img/portfolio/ZEAL.jpg'
-import jetAway from '../../img/portfolio/JetAway.jpg'
-import foodApedia from '../../img/portfolio/foodImg.jpg'
-import weather from '../../img/portfolio/weather.jpg'
-import Notes from '../../img/portfolio/Notes.jpg'
-import scheduler from '../../img/portfolio/planner.jpg'
-// import trivia from '../../img/portfolio/quiz.jpg'
-
+import ECU from '../../img/portfolio/ECUWIT.PNG'
+import gastronomy from '../../img/portfolio/gastronomy.png'
+import book from '../../img/portfolio/book.jpg'
+import generator from '../../img/portfolio/generator.png'
+import schedule from '../../img/portfolio/schedule.jpg'
 import { FaGithub, FaLink } from 'react-icons/fa';
 
 
@@ -29,64 +27,64 @@ function Portfolio() {
               <div class="portfolio-info">
                 <h4>ZEAL</h4>
                 <p>React / Node.js / MongoDB / GraphQL</p>
-                <a target='_blank' rel="noreferrer" href="https://snk923.github.io/React-Portfolio/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/snk923/ZEAL"><FaGithub /></a>
+                <a target='_blank' rel="noreferrer" href="https://zeal3.herokuapp.com/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/snk923/ZEAL"><FaGithub /></a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
             <div class="portfolio-wrap">
-              <img src={jetAway} class="img-fluid" alt="Photographer: Boxed Water"></img>
+              <img src={ECU} class="img-fluid" alt="Photographer: Boxed Water"></img>
               <div class="portfolio-info">
-                <h4>JetAway</h4>
-                <p>HTML / CSS / JavaScript / Node.js / mySQL</p>
-                <a target='_blank' rel="noreferrer" href="https://jetaway1.herokuapp.com/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/snk923/JetAway"><FaGithub /></a>
+                <h4>ECU WIT</h4>
+                <p>HTML / CSS / JavaScript / BoootStrap</p>
+                <a target='_blank' rel="noreferrer" href="https://nvaldovinos.github.io/ECU-WIT/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/Nvaldovinos/ECU-WIT"><FaGithub /></a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
             <div class="portfolio-wrap">
-              <img src={foodApedia} class="img-fluid" alt="Photographer: Cooker King"></img>
+              <img src={gastronomy} class="img-fluid" alt="Photographer: Cooker King"></img>
               <div class="portfolio-info">
-                <h4>FoodApedia</h4>
+                <h4>Globalstranomy</h4>
                 <p>JavaScript / REST APIs</p>
-                <a target='_blank' rel="noreferrer" href="https://santipalacios2002.github.io/foodApedia/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/santipalacios2002/foodApedia"><FaGithub /></a>
+                <a target='_blank' rel="noreferrer" href="https://tmaxey201.github.io/Recipe-with-translator/index.html"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/Tmaxey201/Recipe-with-translator"><FaGithub /></a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 portfolio-item filter-web">
             <div class="portfolio-wrap">
-              <img src={weather} class="img-fluid" alt="Weather Dashboard Photographer: Henruk Donnestad"></img>
+              <img src={book} class="img-fluid" alt="Photographer: Henruk Donnestad"></img>
               <div class="portfolio-info">
-                <h4>Weather Dashboard</h4>
-                <p>JavaScript / APIs</p>
-                <a target='_blank' rel="noreferrer" href="https://snk923.github.io/homework6-weather-dashboard/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/snk923/homework6-weather-dashboard"><FaGithub /></a>
+                <h4>Follow the Reader</h4>
+                <p>BoootStrap /JavaScript / APIs / MySQL</p>
+                <a target='_blank' rel="noreferrer" href="https://follow-the-reader.herokuapp.com/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/AnneliseRowan/project-2"><FaGithub /></a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
             <div class="portfolio-wrap">
-              <img src={Notes} class="img-fluid"
-                alt="NoteTaker Photographer: Charles Deluvio"></img>
+              <img src={generator} class="img-fluid"
+                alt="Photographer: Charles Deluvio"></img>
               <div class="portfolio-info">
-                <h4>NoteTaker</h4>
-                <p>JavaScript / Express.js / Node.js</p>
-                <a target='_blank' rel="noreferrer" href="https://skolb-note-taker.herokuapp.com/notes"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/snk923/homework11-Express.js-NoteTaker"><FaGithub /></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-wrap">
-              <img src={scheduler} class="img-fluid"
-                alt="Workday Scheduler Photographer: Emma Matthews Digital Content"></img>
-              <div class="portfolio-info">
-                <h4>Work Day Scheduler</h4>
+                <h4>ReadMe generator</h4>
                 <p>JavaScript</p>
-                <a target='_blank' rel="noreferrer" href="https://snk923.github.io/homework5-workdayScheduler/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/snk923/homework5-workdayScheduler"><FaGithub /></a>
+                <a target='_blank' rel="noreferrer" href="https://github.com/Nvaldovinos/README-Genetrator-"><FaLink /></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src={schedule} class="img-fluid"
+                alt="Photographer: Emma Matthews Digital Content"></img>
+              <div class="portfolio-info">
+                <h4>Workday Scheduler</h4>
+                <p>JavaScript</p>
+                <a target='_blank' rel="noreferrer" href="https://nvaldovinos.github.io/Work-Day-Scheduler/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/Nvaldovinos/Work-Day-Scheduler/"><FaGithub /></a>
               </div>
             </div>
           </div>
